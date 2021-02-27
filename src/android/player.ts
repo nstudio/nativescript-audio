85810d79bcfd07fc791131be4ff1db0ca5197129
entelostre <polosere22@gmail.com>
2021-02-27 21:38:30 +0200

    fixed tslint issues

--- src/android/player.ts
+++ src/android/player.ts
@@ -211,7 +211,7 @@ export class TNSPlayer implements TNSPlayerI {
           );
 
           if (this._options?.pitch) {
-            let playBackParams = new android.media.PlaybackParams();
+            const playBackParams = new android.media.PlaybackParams();
             playBackParams.setPitch(this._options!.pitch);
             this._player.setPlaybackParams(playBackParams);
           }
